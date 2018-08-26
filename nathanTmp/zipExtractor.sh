9eaaa7fb7298ef573f53785b223b5f4ed6eb19f7
Nathan Oyama <nathan.oyama@berkeley.edu>
2018-08-26 10:30:10 -0600

    added nathan/zipExtractor.sh

---
+++ nathanTmp/zipExtractor.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Version 0.1. Sun Aug 26 10:23:36 DST 2018
+
+# Extracts ZIP file
+# Checks existence of all the required files
+# * <extracted directory name>/virgo/log.log
+
+# Usage: 
+# $ bash virgoZip2Text.sh <system log ZIP file>
+
+if [[ -f $1 ]] && [[ $(unzip -t $1) ]]; then
+    echo "Loaded the ZIP file $1."
+    DIRNAME_UNZIP=$(unzip -l $1 | sed -n 4p | awk -F '[ /]' {'print $11'})
+    if [[ -d $DIRNAME_UNZIP ]]; then
+        echo "Drecttory $DIRNAME_UNZIP already exists. Skip extracting."
+    else
+        echo "Extracting $1."
+        unzip -q $1
+    fi
+elif [[ -d $1 ]]; then
+    echo "Loaded the directory $1."
+    DIRNAME_UNZIP=$1
+else
+    echo -e "$1 is not a valid ZIP file.\n"
+    echo -e "$USAGE"
+    echo "Aborting."
+    exit 1
+fi
+
+PATH_VIRGO=$(echo "./$DIRNAME_UNZIP/virgo/")
+
+ERR_FILE_NOT_EXIST="Please download a new system log again and retry. \
+If you keep seeing this error message, please contact IBM Spectrum Protect \
+Plus Cusotmer Support."
+
+if [[ ! -f PATH_VIRGO/virgo/log.log ]]; then
+    echo "Could not find the file 'virgo/log.log'."
+    echo $ERR_FILE_NOT_EXIST
+    echo "Aborting."
+    exit 1
+fi
+
