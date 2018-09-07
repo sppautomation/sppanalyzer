2ddf95867894b24165b868bbdc168411cfedc112
benjones917 <benjones917@gmail.com>
2018-09-07 10:37:55 -0400

    change to __init__

--- sppanalyzer/__init__.py
+++ sppanalyzer/__init__.py
@@ -2,40 +2,29 @@ import os
 
 from flask import Flask, request
 
+app = Flask(__name__, instance_relative_config=True)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-    )
-    app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# a simple page
+@app.route('/test')
+def hello():
+    return "This is a test"
 
-    # a simple page
-    @app.route('/test')
-    def hello():
-        return "This is a test"
+from . import upload
+app.register_blueprint(upload.bp)
 
-    from . import upload
-    app.register_blueprint(upload.bp)
+from . import unpack
+app.register_blueprint(unpack.bp)
 
-    from . import unpack
-    app.register_blueprint(unpack.bp)
+from . import analyze
+app.register_blueprint(analyze.bp)
 
-    from . import analyze
-    app.register_blueprint(analyze.bp)
-
-    return app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
