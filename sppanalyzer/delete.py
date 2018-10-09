d54f41f35c8ebc599e56165e4a4b3c9186bc77da
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2018-10-08 20:36:07 -0400

    changed message

--- sppanalyzer/delete.py
+++ sppanalyzer/delete.py
@@ -1,7 +1,7 @@
 import os
 import time
 import shutil
-from flask import Blueprint, current_app as app, Response
+from flask import Blueprint, current_app as app, jsonify
 
 bp = Blueprint('delete', __name__, url_prefix='/delete')
 
@@ -14,6 +14,6 @@ def delete_old_logs():
             if os.path.isdir(log_dir):
                 if os.stat(log_dir).st_mtime < now - (app.config["FILE_EXPIRATION_TIME"] * 86400):
                     shutil.rmtree(log_dir, ignore_errors=True)
-        return Response({"message": "Deleted files"}, status=200)
+        return jsonify({"message": "Deleted files", "status": 200})
     except Exception:
-        return Response({"message": "Failed to delete files"}, status=500)
+        return jsonify({"message": "Failed to delete files", "status": 500})
