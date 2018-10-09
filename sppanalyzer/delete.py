462962db8eb5fc6ee48bfb2fbd97904e0ad2d6e6
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2018-10-08 20:32:58 -0400

    returning the proper messages

--- sppanalyzer/delete.py
+++ sppanalyzer/delete.py
@@ -1,15 +1,19 @@
 import os
 import time
 import shutil
-from flask import Blueprint, current_app as app
+from flask import Blueprint, current_app as app, Response
 
 bp = Blueprint('delete', __name__, url_prefix='/delete')
 
 @bp.route("", methods=["OPTIONS", "POST", "GET"])  #Doesn't matter which we end up implementing
 def delete_old_logs():
     now = time.time()
-    for name in os.listdir(app.config["UPLOAD_FOLDER"]):
-        log_dir = os.path.join(app.config["UPLOAD_FOLDER"], name)
-        if os.path.isdir(log_dir):
-            if os.stat(log_dir).st_mtime < now - (app.config["FILE_EXPIRATION_TIME"] * 86400):
-                shutil.rmtree(log_dir, ignore_errors=True)
+    try:
+        for name in os.listdir(app.config["UPLOAD_FOLDER"]):
+            log_dir = os.path.join(app.config["UPLOAD_FOLDER"], name)
+            if os.path.isdir(log_dir):
+                if os.stat(log_dir).st_mtime < now - (app.config["FILE_EXPIRATION_TIME"] * 86400):
+                    shutil.rmtree(log_dir, ignore_errors=True)
+        return Response({"message": "Deleted files"}, status=200)
+    except Exception:
+        return Response({"message": "Failed to delete files"}, status=500)
