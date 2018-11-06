42af0b6c3202d328e886f1c1b02849a6df5ff495
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2018-11-06 14:03:47 -0500

    just reverting

--- sppanalyzer/analyze.py
+++ sppanalyzer/analyze.py
@@ -6,7 +6,7 @@ import subprocess
 from flask import Flask, flash, request, redirect, url_for, Blueprint, render_template, jsonify
 from flask import current_app as app
 from werkzeug.utils import secure_filename
-import parser
+from parser import Parser
 
 bp = Blueprint('analyze', __name__, url_prefix='/analyze')
 
@@ -83,7 +83,8 @@ def virgo_log_exists(virgolog, logdir):
 
 def get_joboverview_data(logdir):
     logfullpath = get_log_fullpath(logdir)
-    return jsonify(get_joboverview_data_py(logfullpath + "/virgo/all_logs.log"))
+    p = Parser(logfullpath)
+    return jsonify(p.get_joboverview_data())
     #check if exists first
     # if not os.path.isfile(os.path.join(logdir, 'virgoLogIndex.csv')):
     #     subprocess.check_call([os.getcwd() + "/sppanalyzer/scripts/virgoCsv.sh",
@@ -125,103 +126,102 @@ def csv_to_json(csvfile):
     return jsonify(dictcsv)
 
 
-import re
 
-catalog_matcher = re.compile("catalog")
-on_demand_restore_matcher = re.compile("onDemandRestore")
-maintenance_matcher = re.compile("Maintenance")
-vmware_matcher = re.compile("vmware_")
-hyper_v_matcher = re.compile("hyperv_")
-oracle_matcher = re.compile("oracle_")
-sql_matcher = re.compile("sql_")
-db2_matcher = re.compile("db2_")
-
-job_pattern = re.compile("[0-9]{13} ===== Starting job for policy .*\. id")
-vm_job_pattern = re.compile("[0-9]{13} vmWrapper .* type vm")
-job_app_pattern = re.compile("[0-9]{13} Options for database .*")
-completion_pattern = re.compile("[0-9]{13} .* completed.*with status .*")
-completion_type_matcher = re.compile("(COMPLETED|PARTIAL|FAILED|RESOURCE ACTIVE)")
-job_id_pattern = re.compile("[0-9]{13}")
-
-def get_timestamp_from_id(job_id):
-    return job_id[:-3]
-
-def job_classifier(job_name):
-    job_type, sla_name = None, None
-    if catalog_matcher.search(job_name):
-        job_type = "SPP"
-        sla_name = "Catalog"
-    elif on_demand_restore_matcher.search(job_name):
-        job_type = "SPP"
-        sla_name = "On-Demand Restore"
-    elif maintenance_matcher.search(job_name):
-        job_type = "SPP"
-        sla_name = "Maintenance"
-    elif vmware_matcher.search(job_name):
-        job_type = "VMware"
-        sla_name = "_".join(job_name.split("_")[1:])
-    elif hyper_v_matcher.search(job_name):
-        job_type = "Hyper-V"
-        sla_name = "_".join(job_name.split("_")[1:])
-    elif oracle_matcher.search(job_name):
-        job_type = "Oracle"
-        sla_name = "_".join(job_name.split("_")[1:])
-    elif sql_matcher.search(job_name):
-        job_type = "SQL"
-        sla_name = "_".join(job_name.split("_")[1:])
-    elif db2_matcher.search(job_name):
-        job_type = "DB2"
-        sla_name = "_".join(job_name.split("_")[1:])
-    else:
-        job_type = "SPP"
-        sla_name = job_name
-    return sla_name, job_type
-
-def get_joboverview_data_py(log_path):
-    universal_dict = {}
-    for line in open(log_path, "r"):
-        line = re.sub("\s+", " ", line)
-        res = job_pattern.search(line)
-        if (res):
-            job_id = res.group(0)[0:13]
-            job_name = " ".join(res.group(0).split(" ")[6:-9])
-            universal_dict[job_id] = {}
-            universal_dict[job_id]["JobID"] = job_id
-            universal_dict[job_id]["StartDateTime"] = get_timestamp_from_id(job_id)
-            universal_dict[job_id]["SLA"], universal_dict[job_id]["JobType"] = job_classifier(job_name)
-            universal_dict[job_id]["Targets"] = ""
-            continue
-        vm = vm_job_pattern.search(line)
-        if vm:
-            key = vm.group(0).split(' ')[0]
-            val = vm.group(0).split(' ')[2]
-            if universal_dict.get(key) is None:
-                universal_dict[key] = {}
-                universal_dict[key]["JobId"] = key
-            universal_dict[key]["Targets"] = universal_dict[key]["Targets"] + f":{val}" if universal_dict[key][
-                "Targets"] else f"{val}"
-            continue
-        job_apps = job_app_pattern.search(line)
-        if job_apps:
-            key = job_apps.group(0).split(' ')[0]
-            val = job_apps.group(0).split(' ')[4][0:-3]
-            if universal_dict.get(key) is None:
-                universal_dict[key] = {}
-                universal_dict[key]["JobId"] = key
-            universal_dict[key]["Targets"] = universal_dict[key]["Targets"] + f":{val}" if universal_dict[key][
-                "Targets"] else f"{val}"
-            continue
-        completion = completion_pattern.search(line)
-        if completion:
-            completion_type = completion_type_matcher.search(line)
-            key = job_id_pattern.search(line).group(0)
-            if universal_dict.get(key) is None:
-                universal_dict[key] = {}
-                universal_dict[key]["JobId"] = key
-            if completion_type:
-                universal_dict[key]["Result"] = completion_type.group(0)
-            else:
-                universal_dict[key]["Result"] = "UNKNOWN"
-
-    ret = [universal_dict[i] for i in universal_dict]
-    return ret
+# catalog_matcher = re.compile("catalog")
+# on_demand_restore_matcher = re.compile("onDemandRestore")
+# maintenance_matcher = re.compile("Maintenance")
+# vmware_matcher = re.compile("vmware_")
+# hyper_v_matcher = re.compile("hyperv_")
+# oracle_matcher = re.compile("oracle_")
+# sql_matcher = re.compile("sql_")
+# db2_matcher = re.compile("db2_")
+#
+# job_pattern = re.compile("[0-9]{13} ===== Starting job for policy .*\. id")
+# vm_job_pattern = re.compile("[0-9]{13} vmWrapper .* type vm")
+# job_app_pattern = re.compile("[0-9]{13} Options for database .*")
+# completion_pattern = re.compile("[0-9]{13} .* completed.*with status .*")
+# completion_type_matcher = re.compile("(COMPLETED|PARTIAL|FAILED|RESOURCE ACTIVE)")
+# job_id_pattern = re.compile("[0-9]{13}")
+#
+# def get_timestamp_from_id(job_id):
+#     return job_id[:-3]
+#
+# def job_classifier(job_name):
+#     job_type, sla_name = None, None
+#     if catalog_matcher.search(job_name):
+#         job_type = "SPP"
+#         sla_name = "Catalog"
+#     elif on_demand_restore_matcher.search(job_name):
+#         job_type = "SPP"
+#         sla_name = "On-Demand Restore"
+#     elif maintenance_matcher.search(job_name):
+#         job_type = "SPP"
+#         sla_name = "Maintenance"
+#     elif vmware_matcher.search(job_name):
+#         job_type = "VMware"
+#         sla_name = "_".join(job_name.split("_")[1:])
+#     elif hyper_v_matcher.search(job_name):
+#         job_type = "Hyper-V"
+#         sla_name = "_".join(job_name.split("_")[1:])
+#     elif oracle_matcher.search(job_name):
+#         job_type = "Oracle"
+#         sla_name = "_".join(job_name.split("_")[1:])
+#     elif sql_matcher.search(job_name):
+#         job_type = "SQL"
+#         sla_name = "_".join(job_name.split("_")[1:])
+#     elif db2_matcher.search(job_name):
+#         job_type = "DB2"
+#         sla_name = "_".join(job_name.split("_")[1:])
+#     else:
+#         job_type = "SPP"
+#         sla_name = job_name
+#     return sla_name, job_type
+#
+# def get_joboverview_data_py(log_path):
+#     universal_dict = {}
+#     for line in open(log_path, "r"):
+#         line = re.sub("\s+", " ", line)
+#         res = job_pattern.search(line)
+#         if (res):
+#             job_id = res.group(0)[0:13]
+#             job_name = " ".join(res.group(0).split(" ")[6:-9])
+#             universal_dict[job_id] = {}
+#             universal_dict[job_id]["JobID"] = job_id
+#             universal_dict[job_id]["StartDateTime"] = get_timestamp_from_id(job_id)
+#             universal_dict[job_id]["SLA"], universal_dict[job_id]["JobType"] = job_classifier(job_name)
+#             universal_dict[job_id]["Targets"] = ""
+#             continue
+#         vm = vm_job_pattern.search(line)
+#         if vm:
+#             key = vm.group(0).split(' ')[0]
+#             val = vm.group(0).split(' ')[2]
+#             if universal_dict.get(key) is None:
+#                 universal_dict[key] = {}
+#                 universal_dict[key]["JobId"] = key
+#             universal_dict[key]["Targets"] = universal_dict[key]["Targets"] + f":{val}" if universal_dict[key][
+#                 "Targets"] else f"{val}"
+#             continue
+#         job_apps = job_app_pattern.search(line)
+#         if job_apps:
+#             key = job_apps.group(0).split(' ')[0]
+#             val = job_apps.group(0).split(' ')[4][0:-3]
+#             if universal_dict.get(key) is None:
+#                 universal_dict[key] = {}
+#                 universal_dict[key]["JobId"] = key
+#             universal_dict[key]["Targets"] = universal_dict[key]["Targets"] + f":{val}" if universal_dict[key][
+#                 "Targets"] else f"{val}"
+#             continue
+#         completion = completion_pattern.search(line)
+#         if completion:
+#             completion_type = completion_type_matcher.search(line)
+#             key = job_id_pattern.search(line).group(0)
+#             if universal_dict.get(key) is None:
+#                 universal_dict[key] = {}
+#                 universal_dict[key]["JobId"] = key
+#             if completion_type:
+#                 universal_dict[key]["Result"] = completion_type.group(0)
+#             else:
+#                 universal_dict[key]["Result"] = "UNKNOWN"
+#
+#     ret = [universal_dict[i] for i in universal_dict]
+#     return ret
