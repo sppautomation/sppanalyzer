00205f3ead17072d58b88ad2d87e90ddea8667ac
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2018-11-14 01:30:53 -0500

    more prints'

--- sppanalyzer/analyze.py
+++ sppanalyzer/analyze.py
@@ -99,8 +99,8 @@ def get_jobdetails_data(logdir, jobsession):
                                 stdout=subprocess.PIPE).communicate()
     loglines = out.decode('utf-8').splitlines()
     for i, line in enumerate(loglines, 0):
+        print(line)
         while not loglines[i].startswith("["):
-            print(loglines)
             loglines[i-1] = loglines[i-1] + "\n" + loglines[i]
             del loglines[i]
     for i, line in enumerate(loglines, 0):
